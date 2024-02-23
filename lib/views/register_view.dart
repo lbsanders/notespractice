3912bd1cadca0c315de6e312372e44be7b2834d0
Lori Sanders <lori.sanders@me.com>
2024-02-23 09:25:49 +0100

    step-16

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -5,8 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:notespractice/constants/routes.dart';
 import 'package:notespractice/services/auth/auth_service.dart';
 import 'package:notespractice/services/auth/auth_exceptions.dart';
-
-import '../utilities/show_error_dialog.dart';
+import '../utilities/dialogs/error_dialog.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
